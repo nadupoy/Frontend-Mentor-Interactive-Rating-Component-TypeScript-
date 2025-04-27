d358bff586c6a53b3c0f7d3a1a4da02191724ac1
g_sampao <gnsampao@gmail.com>
2025-04-27 13:00:05 +0300

    Created markup & styling for '.thank-you-state'.

--- src/elements.ts
+++ src/elements.ts
@@ -1,4 +1,5 @@
 import iconStar from "/images/icon-star.svg";
+import illustrationThankYou from "/images/illustration-thank-you.svg";
 
 export interface Element {
   content: string;
@@ -21,11 +22,28 @@ const formDefault: Element = {
 
 export const section: Element = {
   content: `
+  <div class="default-state">
     <img src=${iconStar}>
     <h1>How did we do?</h1>
     <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
     ${formDefault.content}
+  </div>
     `,
+};
+
+export const thankYouState: Element = {
+  content: `
+  <div class="thank-you-state">
+    <img src=${illustrationThankYou} alt="illustration-thank-you" class="thank-you-state">
+    <p class="rating">
+  You selected 4 out of 5
+    </p>
+    <h1 class="thank-you-state">Thank you!</h1>
+    <p class="thank-you-state">
+  We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+    </p>
+  </div>
+  `,
 };
 
 export const footer: Element = {

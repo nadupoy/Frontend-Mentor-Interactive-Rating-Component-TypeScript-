fe1166c93f9a028a767b3da85966e6b9ffe8bb12
g_sampao <gnsampao@gmail.com>
2025-04-27 13:03:23 +0300

    Refactored code.

--- src/rating-component.ts
+++ src/rating-component.ts
@@ -2,7 +2,9 @@ import { Element, section, footer } from "./elements";
 
 export const ratingComponent: Element = {
   content: `
+  <section>
     ${section.content}
+  </section>
     ${footer.content}
     `,
 };

3d2c06af3e84e32b3ab4b3021f66cc5864c8b52f
g_sampao <gnsampao@gmail.com>
2025-05-01 14:20:47 +0300

    Refactored code for use without using Vite to focus on learning and using vanilla TypeScript. Code was getting confusing, wanted to simplify the process.

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import './style.css'
-import { ratingComponent } from "./rating-component";
-
-// The non-null Assertion Operator (Postfix !) is used here 👇🏽:
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = ratingComponent.content;

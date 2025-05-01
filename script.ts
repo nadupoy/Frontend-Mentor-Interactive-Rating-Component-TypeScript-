025b885f5f5b2de50fd36d38150a1df34f8d35a6
g_sampao <gnsampao@gmail.com>
2025-05-01 20:17:58 +0300

    Selected rating won't output on form submission.

--- script.ts
+++ script.ts
@@ -1,17 +1,42 @@
 // Form:
 const ratingForm = document.getElementById("rating");
 
-// Value of selected rating from the form:
-let rating;
+// Rating buttons:
+const ratingButtons = document.querySelectorAll('[type="button"]');
+
+let result = document.getElementById('rating-score');
 
 // Contents of section.default-state when the rating is selected and form submitted:
 const thankYouState: string = `
 <img src="./images/illustration-thank-you.svg" alt="thank-you-illustration" />
 <p class="rating">
-You selected ${rating} out of 5
+You selected <span id="rating-score"></span> out of 5
 </p>
 <h1>Thank you!</h1>
 <p>
 We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
 </p>
 `;
+
+// Function that picks the value of the selected (in focus) rating button:
+function selectRating() {
+  let rating = this!.value;
+  // Non-null assertion operator:
+  result!.textContent = rating;
+  console.log(rating);
+}
+
+// Add selectRating() to each rating button:
+for (let ratingButton of ratingButtons) {
+  ratingButton.addEventListener("focus", selectRating);
+}
+
+// Element content to be replaced on form submission:
+const submittedForm = document.getElementById("default-component");
+
+function submitRating(event) {
+  submittedForm!.innerHTML = thankYouState;
+  event.preventDefault();
+}
+
+ratingForm!.addEventListener("submit", submitRating);
